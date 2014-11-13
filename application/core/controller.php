f75574a9ce95a21ec6be75b49e51843a1d074ebf
Mazdai57 <alexdvk@gmail.com>
2014-11-13 23:33:16 +0300

    public field change on private
    public field change on private

--- application/core/controller.php
+++ application/core/controller.php
@@ -2,14 +2,22 @@
 
 class Controller {
     
-    public $model;
-    public $view;
+    private $model;
+    private $view;
     
     function __construct() {
         $this->view = new View();
     }
     
     function action_index() {
+    }
+    
+    public function getModel () {
+        return $this->model;
+    }
+    
+    public function getView () {
+        return $this->view;
     }
 }
 
